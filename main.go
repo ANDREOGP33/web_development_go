40590d9efd1dfd0c5ed238923d6a1235b854e8d8
GenesisClinux <desenvolvimento@clinux.com.br>
2024-08-28 20:27:50 -0300

    Using Chi

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/getNome/{nome}", getnome)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", r)
+}
+
+func getnome(w http.ResponseWriter, r *http.Request) {
+	nome := chi.URLParam(r, "nome")
+	fmt.Fprint(w, "Bom dia "+nome)
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,12 +33,12 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "page not found", http.StatusNotFound)
-
 	}
 }
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -35,4 +52,25 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//2.8 The http.Handler Type
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+	<li>
+	<b>Is there a free version?</b>
+	Yes! We offer a free trial for 30 days on any paid plans.
+</li>
+<li>
+<b>What are your support hours?</b>
+We have support staff answering emails 24/7, though response
+times may be a bit slower on weekends.
+</li>
+<li>
+<b>How do I contact support?</b>
+Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+</li>
+</ul>
+`)
+}
+
+//81
